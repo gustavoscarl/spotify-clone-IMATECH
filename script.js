3d0663e9cd91bafd3eaebb44cfc19329f329cc24
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2023-11-14 15:11:27 -0300

    alot of changes

---
+++ script.js
@@ -0,0 +1,26 @@
+// Menu Dropdown
+const icon = document.querySelector('.hamburger-menu')
+const list = document.querySelector('.mobile')
+
+icon.addEventListener('click', () => {
+  if (list.classList.contains('inactive')) {
+    list.classList.remove('inactive');
+    list.classList.add('active');
+  } else {
+    list.classList.add('inactive');
+    list.classList.remove('active')
+  } 
+});
+
+// Close Dropdown when Window > 601px
+
+function closeDropdown(sizeWindow){
+  if (sizeWindow.matches && list.classList.contains('active')) {
+    list.classList.remove('active');
+    list.classList.add('inactive');
+  }
+}
+
+let sizeWindow = window.matchMedia("(min-width:601px)");
+
+window.addEventListener('resize', () => {closeDropdown(sizeWindow)})
